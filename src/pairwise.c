a5832ea944b64a81459e1ce520d9a1465d3584ce
greg <greg@gregs-iMac.local>
2025-04-14 16:44:19 -0400

    upd loglike

--- src/pairwise.c
+++ src/pairwise.c
@@ -2495,8 +2495,7 @@ int Likelihood_Pairwise (int division, int chain, MrBFlt *lnL)
                     return ERROR;
                     }
 
-                if (m->usePwWeights)
-                    like=like*m->pwWeight*(2.0/(numLocalTaxa * (numLocalTaxa-1)));
+                like=like*(2.0 / (numLocalTaxa * (numLocalTaxa-1)));
 
                 (*lnL)+=like;
 
