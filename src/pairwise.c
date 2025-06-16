1d83960d153c1ec487122c137c8c1835bc7bf1ce
greg <greg@gregs-iMac.local>
2025-06-16 19:26:47 -0400

    numerical deriv

--- src/pairwise.c
+++ src/pairwise.c
@@ -3503,7 +3503,6 @@ int TranProbMatrix_GTR(ModelInfo *m, int chain, double dist, double al, double *
     return(1);
 }
 
-
 int CalcPairwiseWeights_GTR (int chain) {
 
     /*  setup the arays for pairwise counts,
@@ -3545,7 +3544,9 @@ int CalcPairwiseWeights_GTR (int chain) {
     int freeBitsets;
     int *tempPartitionPair;
 
-    MrBFlt h=0.0000001;
+    //MrBFlt h=MRBFLT_MIN;
+    MrBFlt h=ETA;
+
     MrBFlt *bs;
 
     /*  first set up worker matrices for eigen computation */
@@ -3909,7 +3910,7 @@ int CalcPairwiseWeights_GTR (int chain) {
                     TranProbMatrix_GTR(m, chain, dist+h, al, tptemp);
                     for (i=0;i<4;i++)
                         for (j=0;j<4;j++)
-                            tp1[k][dIdx(i,j,4)] = (tp[k][dIdx(i,j,4)] - tptemp[dIdx(i,j,4)])/h  ;
+                            tp1[k][dIdx(i,j,4)] = (tp[k][dIdx(i,j,4)] - tptemp[dIdx(i,j,4)]) /   ;
                     }
                 }
             }
