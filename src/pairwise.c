1ab5c90ec996c649638c43ed4d301fac0b140173
greg <greg@gregs-iMac.local>
2025-08-14 20:20:39 -0400

    jc base freqs

--- src/pairwise.c
+++ src/pairwise.c
@@ -2302,8 +2302,6 @@ int TiProbsPairwise_Gen (int division, int chain)
 
 int DoubletProbs_JukesCantor(int division, int chain)
 {
-
-    
     int         i, j, k, p, index, dpIdx;
     CLFlt       *tiP, *doubP;   
     ModelInfo   *m;
@@ -2335,7 +2333,7 @@ int DoubletProbs_JukesCantor(int division, int chain)
             dpIdx=0;
             for (i=0; i<4; i++)
                 for (j=0; j<4; j++)
-                    doubP[dpIdx++] += bs[i] * tiP[index++]/((MrBFlt)m->numRateCats);
+                    doubP[dpIdx++] += 0.25 * tiP[index++]/((MrBFlt)m->numRateCats);
             }
         }
     return(NO_ERROR);
@@ -2606,6 +2604,7 @@ MrBFlt LogLikePairwise(int chain)
 
     return(chainLnLike);
 }
+
 MrBFlt EstPwDist_JC(int p) 
 {
     MrBFlt tau;
